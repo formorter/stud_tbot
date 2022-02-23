9ee95840537a0112c8fc8ac2ed243b4f094676b8
twipz333 <53153082+twipz333@users.noreply.github.com>
2022-02-23 18:49:04 +0300

    Revert "Update app.py"
    This reverts commit 94ee08e276b761e56e54cd9cebd508a286467d59.

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
 
 if __name__ == '__main__':
-    asyncio.run(handlers.group.event.periodic(60))
+    loop = asyncio.get_event_loop()
+    loop.create_task(handlers.group.event.periodic(60))
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
