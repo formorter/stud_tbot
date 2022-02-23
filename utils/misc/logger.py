fbb9824030682a7c2b2048879dbe3c1177bc1ae0
twipz333 <53153082+twipz333@users.noreply.github.com>
2022-02-23 20:54:26 +0300

    Update logger.py

--- utils/misc/logger.py
+++ utils/misc/logger.py
@@ -7,7 +7,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 log_format = logging.Formatter('%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s')
 
-file_handler = RotatingFileHandler(filename=f'./data/log/log.txt', mode='a', maxBytes=500*1024*1024, backupCount=1,encoding='utf-8')
+file_handler = RotatingFileHandler(filename=f'./data/log/log-{int(time())}.txt', mode='a', maxBytes=500*1024*1024, backupCount=1,encoding='utf-8')
 file_handler.setFormatter(log_format)
 file_handler.setLevel(logging.INFO)
 logger.addHandler(file_handler)
