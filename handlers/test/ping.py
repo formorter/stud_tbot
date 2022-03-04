7adb3542de7d4bd9f1cbed6e07d2ab293720f74e
twipz333 <53153082+twipz333@users.noreply.github.com>
2022-03-04 19:04:16 +0300

    Update ping.py
    /schedule upgrade

--- handlers/test/ping.py
+++ handlers/test/ping.py
@@ -14,11 +14,37 @@ async def ping_command(message: types.Message):
 @dp.message_handler(commands=['schedule'])
 async def schedule_command(message: types.Message):
     day = datetime.strftime(datetime.now(), '%A')
+    week = is_upper_week()
     sep = '\n' + '-' * 20 + '\n'
     schedule = []
+    
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday', 'Saturday']
+    weeks = ['Верхняя', 'Нижняя']
+
+    args = message.get_args()
+    
+    if isinstance(args, str):
+        args = args.split()
+    if args:
+        if len(args) == 2:
+            if args[0] in days:
+                if args[1] in weeks:
+                    day = args[0]
+                    week = True if args[1] == 'Верхняя' else False
+                    schedule.append(f'Расписание на {args[0]}. Неделя: {args[1]}')
+                else:
+                    day = args[0]
+                    schedule.append(f'Неверные входные данные! Выведено расписание на {args[0]} текущей недели!')
+            else:
+                schedule.append('Неверные входные данные! Выведено расписание на сегодня')
+        elif args[0] in days:
+            day = args[0]
+            schedule.append(f'Расписание на {args[0]} текущей недели')
+
     for record in Schedule.select():
-        if record.day == day and record.isUpperWeek == is_upper_week():
-            schedule.append(f'{record.time} - {record.name}')
+        if record.day == day and record.isUpperWeek == week:
+            schedule.append(f'{record.schedule_id}. {record.time} - {record.name}')
+
 
     await bot.send_message(message.chat.id, sep.join(schedule))
     log.info('Schedule command from {}'.format(message.from_user.username))
